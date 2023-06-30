8170aae1468939f604e49e695ed136a7ddc4e9f9
Var4games <134076573+Var4games@users.noreply.github.com>
2023-06-30 21:57:56 +0530

    Update page.js

--- page.js
+++ page.js
@@ -1,13 +1,1 @@
-import Image from 'next/image'
-import Navbar from '@/components/navbar'
-import Topbar from '@/components/topbar'
-export default function Home() {
-  return (
-    <main >
-      <div >
-       <Navbar></Navbar>
-       <Topbar></Topbar>
-      </div>
-    </main>
-  )
-}
+
